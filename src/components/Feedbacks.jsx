c73a5812b9bf46e4182bc4d35f6c75484ddb3ef1
meena-gang <145905825+meena-gang@users.noreply.github.com>
2024-07-20 17:55:12 +0530

    Add files via upload

---
+++ src/components/Feedbacks.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+import { testimonials } from "../constants";
+
+const FeedbackCard = ({
+  index,
+  image,
+}) => (
+  <motion.div
+    variants={fadeIn("", "spring", index * 0.5, 0.75)}
+    className='bg-black-200 p-10 rounded-3xl xs:w-[100%] w-full'
+  >
+    <div className='mt-1'>
+      <div className='mt-7 flex justify-between items-center gap-1'>
+        <img
+          src={image}
+          alt={"Github_Profile"}
+          className='w-full h-80 rounded-3xl'
+        />
+      </div>
+    </div>
+  </motion.div>
+);
+
+const Feedbacks = () => {
+  return (
+    <div className={`mt-12 bg-black-100 rounded-[20px]`}>
+      <div
+        className={`bg-tertiary rounded-2xl ${styles.padding} min-h-[300px]`}
+      >
+        <motion.div variants={textVariant()}>
+          <p className={styles.sectionSubText}>Git Stat's</p>
+        </motion.div>
+      </div>
+      <div className={`-mt-20 pb-14 ${styles.paddingX} grid gap-7`}>
+        {testimonials.map((testimonial, index) => (
+          <FeedbackCard key={testimonial.name} index={index} {...testimonial} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SectionWrapper(Feedbacks, "githubStats");
